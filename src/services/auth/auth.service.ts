845bad1474c8526b3377d91d4075541dfa49924e
ShuVeriDa <bashtarov@outlook.com>
2023-03-13 20:51:21 +0300

    refactored. Added api helper and interceptor, auth helper and service, also types

---
+++ src/services/auth/auth.service.ts
@@ -0,0 +1,32 @@
+import {IAuthResponse, IEmailPassword} from "@/store/user/user.interface";
+import {saveToStorage} from "@/services/auth/auth.helper";
+import axios from "axios";
+import Cookies from "js-cookie";
+import {instance} from "@/api/api.interceptor";
+
+export const AuthService = {
+  async main(type: 'login' | 'register', data: IEmailPassword) {
+    const response = await instance<IAuthResponse>({
+      url: `/auth/${type}`,
+      method: 'POST',
+      data
+    })
+
+    if (response.data.accessToken) saveToStorage(response.data)
+
+    return response.data
+  },
+
+  async getNewTokens() {
+    const refreshToken = Cookies.get('refreshToken')
+
+    const response = await axios.post<string, { data: IAuthResponse }>(
+      '/auth/login/access-token',
+      { refreshToken }
+    )
+
+    if (response.data.accessToken) saveToStorage(response.data)
+
+    return response
+  }
+}
